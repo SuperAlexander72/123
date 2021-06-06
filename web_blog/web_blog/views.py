9264eb1b2839ca3e66b0dd4eb7d5a40d1ac68441
Alexsandr <alex.matveev.7.11@mail.ru>
2021-06-06 13:20:11 +0700

    Kall

--- web_blog/web_blog/views.py
+++ web_blog/web_blog/views.py
@@ -4,7 +4,7 @@ import json
 from django.shortcuts import render,redirect
 import database
 
-url = 'http://123.herokuapp.com/'
+url = 'http://web-blog-ok.herokuapp.com/'
 
 @csrf_exempt
 def index(request):
